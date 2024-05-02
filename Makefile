00dcc10f6311138942845dde323728a376aa772f
Alec Savvy <alecsavvy@outlook.com>
2024-05-02 09:52:46 -0600

    initial docker setup with one node

--- Makefile
+++ Makefile
@@ -1,37 +1,12 @@
-GOCMD=go
-GOBUILD=$(GOCMD) build
-GORUN=$(GOCMD) run
-BINARY_NAME=myapp
+default:
+	make down
+	make up
 
-all: build run
+up:
+	docker compose up --build -d
 
-build:
-	make gen
-	$(GOBUILD) -o $(BINARY_NAME) main.go
+down:
+	docker compose down
 
-run:
-	./$(BINARY_NAME)
-
-clean:
-	rm -f $(BINARY_NAME)
-
-deps:
-	go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
-	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-
-
-gen:
-	cd ./db/sql && sqlc generate
-	cd ./grpc/proto && protoc --proto_path=. --go_out=../../ --go-grpc_out=../../ *.proto
-
-# dev cluster
-
-node1:
-	$(GORUN) main.go --config=./dev_config/node_1.toml
-
-node2:
-	$(GORUN) main.go --config=./dev_config/node_2.toml
-
-node3:
-	$(GORUN) main.go --config=./dev_config/node_3.toml
+init:
+	cometbft init --home ./cmt-home
--- main.go
+++ main.go
@@ -2,35 +2,95 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
 
-	"github.com/alecsavvy/clockwise/common"
+	"github.com/cometbft/cometbft/p2p"
+	"github.com/cometbft/cometbft/privval"
+	"github.com/cometbft/cometbft/proxy"
+
+	cfg "github.com/cometbft/cometbft/config"
+	cmtflags "github.com/cometbft/cometbft/libs/cli/flags"
+	cmtlog "github.com/cometbft/cometbft/libs/log"
+	nm "github.com/cometbft/cometbft/node"
+	"github.com/dgraph-io/badger/v4"
+	"github.com/spf13/viper"
 )
 
-func run() error {
-	logger, err := common.NewLogger()
-	if err != nil {
-		return err
-	}
-
-	configPath := flag.String("config", "./clockwise.toml", "path to configuration file")
-	flag.Parse()
-
-	config, err := common.ReadConfig(*configPath)
-	if err != nil {
-		return err
-	}
-
-	app, err := NewApp(logger, config)
-	if err != nil {
-		return err
-	}
-
-	return app.Run()
-}
-
 func main() {
-	if err := run(); err != nil {
-		log.Fatalf("clockwise crashed: %s", err)
+	flag.Parse()
+	homeDir := "./cmt-home"
+
+	config := cfg.DefaultConfig()
+	config.SetRoot(homeDir)
+	viper.SetConfigFile(fmt.Sprintf("%s/%s", homeDir, "config/config.toml"))
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Reading config: %v", err)
 	}
+	if err := viper.Unmarshal(config); err != nil {
+		log.Fatalf("Decoding config: %v", err)
+	}
+	if err := config.ValidateBasic(); err != nil {
+		log.Fatalf("Invalid configuration data: %v", err)
+	}
+	dbPath := filepath.Join(homeDir, "badger")
+	db, err := badger.Open(badger.DefaultOptions(dbPath))
+
+	if err != nil {
+		log.Fatalf("Opening database: %v", err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("Closing database: %v", err)
+		}
+	}()
+
+	app := NewKVStoreApplication(db)
+
+	pv := privval.LoadFilePV(
+		config.PrivValidatorKeyFile(),
+		config.PrivValidatorStateFile(),
+	)
+
+	nodeKey, err := p2p.LoadNodeKey(config.NodeKeyFile())
+	if err != nil {
+		log.Fatalf("failed to load node's key: %v", err)
+	}
+
+	logger := cmtlog.NewTMLogger(cmtlog.NewSyncWriter(os.Stdout))
+	logger, err = cmtflags.ParseLogLevel(config.LogLevel, logger, cfg.DefaultLogLevel)
+
+	if err != nil {
+		log.Fatalf("failed to parse log level: %v", err)
+	}
+
+	node, err := nm.NewNode(
+		config,
+		pv,
+		nodeKey,
+		proxy.NewLocalClientCreator(app),
+		nm.DefaultGenesisDocProviderFunc(config),
+		cfg.DefaultDBProvider,
+		nm.DefaultMetricsProvider(config.Instrumentation),
+		logger,
+	)
+
+	if err != nil {
+		log.Fatalf("Creating node: %v", err)
+	}
+
+	node.Start()
+	defer func() {
+		node.Stop()
+		node.Wait()
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
 }
