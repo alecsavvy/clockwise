76add5605a59fe29aff7d0ecd47b103fb0873834
Alec Savvy <alecjsavvy@gmail.com>
2024-05-25 10:29:51 -0600

    loadtest works AHHH

--- loadtest/main.go
+++ loadtest/main.go
@@ -1,44 +1,110 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"math/rand/v2"
+	"net/http"
+	"sync"
+	"text/template"
 	"time"
 
 	"github.com/alecsavvy/clockwise/sdk"
 	"github.com/alecsavvy/clockwise/utils"
 	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 )
+
+//go:embed templates/*
+var embeddedFiles embed.FS
+var tmpl *template.Template
+
+// config
+var interval = 500
+var parallelRequests = 3
+var statsBuffer = 100
+
+func init() {
+	var err error
+	tmpl, err = template.ParseFS(embeddedFiles, "templates/stats_template.html")
+	if err != nil {
+		panic(err)
+	}
+}
 
 // sends random entity manager transations to random nodes
 func main() {
 	logger := utils.NewLogger(nil)
-	for {
-		sdk := sdk.NewSdk(fmt.Sprintf("%s/query", randomDiscprov()))
 
-		requestId := uuid.NewString()
-		userId := randomIntID()
-		entityId := randomIntID()
-		signer := uuid.NewString()
-		entityType := randomEntity()
-		action := randomAction()
-		metadata := "metadata"
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-		_, err := sdk.ManageEntity(
-			requestId,
-			userId,
-			signer,
-			entityType,
-			entityId,
-			metadata,
-			action,
-		)
+	stats := NewStats()
 
-		if err != nil {
-			logger.Error("error sending manage entity", "error", err)
+	go func() {
+		defer wg.Done()
+		for {
+			for i := 0; i < parallelRequests; i++ {
+				go sendRandomRequest(logger, stats)
+			}
+			time.Sleep(time.Duration(interval) * time.Millisecond)
 		}
-		time.Sleep(1 * time.Second)
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		htmlFS, err := fs.Sub(embeddedFiles, "templates")
+		if err != nil {
+			logger.Error("could not read embedded template files", "error", err)
+			return
+		}
+		htmlTemplates := echo.WrapHandler(http.FileServerFS(htmlFS))
+
+		e := echo.New()
+		e.HideBanner = true
+		e.GET("/stats", stats.statsHandler)
+		e.GET("/", htmlTemplates)
+
+		err = e.Start("0.0.0.0:8080")
+		if err != nil {
+			logger.Error("server crashed", "error", err)
+			return
+		}
+	}()
+
+	wg.Wait()
+}
+
+func sendRandomRequest(logger *utils.Logger, stats *Stats) {
+	node := randomDiscprov()
+	sdk := sdk.NewSdk(fmt.Sprintf("%s/query", node))
+
+	requestId := uuid.NewString()
+	userId := randomIntID()
+	entityId := randomIntID()
+	signer := uuid.NewString()
+	entityType := randomEntity()
+	action := randomAction()
+	metadata := "metadata"
+
+	_, err := sdk.ManageEntity(
+		requestId,
+		userId,
+		signer,
+		entityType,
+		entityId,
+		metadata,
+		action,
+	)
+
+	wasError := err != nil
+
+	if wasError {
+		logger.Error("error sending manage entity", "error", err)
 	}
+	stats.recordStat(node, wasError)
 }
 
 var discprovUrls = []string{"http://node-0:26659", "http://node-1:26659", "http://node-2:26659", "http://node-3:26659", "http://node-4:26659", "http://node-5:26659", "http://node-6:26659"}
---
+++ loadtest/stats.go
@@ -0,0 +1,64 @@
+// webserver for the loadtest that shows the state of the loadtest
+// this is all kept in memory so likely won't scale well lol
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type Stats struct {
+	TotalSuccessfulTransactions int
+	TotalFailedTransactions     int
+	TotalNodeCount              int
+	TransactionIntervalMillis   int
+	ParallelRequests            int
+	NodeTotalAttempted          map[string]int
+	NodeTotalSuccesses          map[string]int
+	NodeTotalFailures           map[string]int
+	updateChan                  chan StatsUpdate
+}
+
+type StatsUpdate struct {
+	Node     string
+	WasError bool
+}
+
+func NewStats() *Stats {
+	stats := &Stats{
+		TotalSuccessfulTransactions: 0,
+		TotalNodeCount:              len(discprovUrls),
+		TransactionIntervalMillis:   interval,
+		ParallelRequests:            parallelRequests,
+		NodeTotalAttempted:          map[string]int{},
+		NodeTotalSuccesses:          map[string]int{},
+		NodeTotalFailures:           map[string]int{},
+		updateChan:                  make(chan StatsUpdate, statsBuffer),
+	}
+	go stats.runUpdater()
+	return stats
+}
+
+// run the update listener
+func (s *Stats) runUpdater() {
+	for update := range s.updateChan {
+		if update.WasError {
+			s.NodeTotalAttempted[update.Node]++
+			s.NodeTotalFailures[update.Node]++
+			s.TotalFailedTransactions++
+		} else {
+			s.NodeTotalAttempted[update.Node]++
+			s.NodeTotalSuccesses[update.Node]++
+			s.TotalSuccessfulTransactions++
+		}
+	}
+}
+
+// send update down channel
+func (s *Stats) recordStat(node string, wasError bool) {
+	s.updateChan <- StatsUpdate{Node: node, WasError: wasError}
+}
+
+// route that serves the stats ui
+func (s *Stats) statsHandler(c echo.Context) error {
+	return tmpl.ExecuteTemplate(c.Response().Writer, "stats", s)
+}
