7899e951d83a9f34f2a125f084a7fe5f7b892af7
alecsavvy <16739903+alecsavvy@users.noreply.github.com>
2024-07-19 11:07:15 -0600

    more loadtest goodness (#9)
    * error handling middleware
    
    * loadtest multiple types

--- loadtest/main.go
+++ loadtest/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"io/fs"
-	"math/rand/v2"
 	"net/http"
 	"sync"
 	"text/template"
-	"time"
 
-	"github.com/alecsavvy/clockwise/sdk"
 	"github.com/alecsavvy/clockwise/utils"
-	"github.com/bxcodec/faker/v3"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/sync/errgroup"
 )
@@ -22,10 +17,11 @@ var embeddedFiles embed.FS
 var stats_templ *template.Template
 var block_templ *template.Template
 var health_templ *template.Template
+var error_templ *template.Template
 
 // config
 var interval = 250
-var parallelRequests = 10
+var parallelRequests = 5
 var statsBuffer = 100000
 
 func init() {
@@ -44,6 +40,11 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+
+	error_templ, err = template.ParseFS(embeddedFiles, "templates/error_template.html")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -57,24 +58,14 @@ func main() {
 	go func() {
 		defer wg.Done()
 		for {
-			start := time.Now()
-		
 			var g errgroup.Group
 			for i := 0; i < parallelRequests; i++ {
 				g.Go(func() error {
-					return sendRandomRequest(logger, stats)
+					return testSequence(stats)
 				})
 			}
-	
 			if err := g.Wait(); err != nil {
-				logger.Error("Error in request:", err)
-			}
-	
-			elapsed := time.Since(start)
-			sleepDuration := time.Duration(interval)*time.Millisecond - elapsed
-	
-			if sleepDuration > 0 {
-				time.Sleep(sleepDuration)
+				logger.Error("something blew up", "error", err)
 			}
 		}
 	}()
@@ -95,6 +86,7 @@ func main() {
 		e.GET("/health_stats", getHealthStats)
 		e.POST("/get_block", getBlock)
 		e.GET("/", htmlTemplates)
+		e.Use(errorMiddleware)
 
 		err = e.Start("0.0.0.0:8080")
 		if err != nil {
@@ -106,34 +98,14 @@ func main() {
 	wg.Wait()
 }
 
-func sendRandomRequest(_ *utils.Logger, stats *Stats) error {
-	node := randomDiscprov()
-	sdk := sdk.NewSdk(fmt.Sprintf("%s/query", node))
-
-	account, _ := generateWallet()
-
-	handle := faker.Username()
-	address := account.Address.Hex()
-	bio := faker.Sentence()
-
-	_, err := sdk.CreateUser(
-		handle,
-		address,
-		bio,
-	)
-
-	wasError := err != nil
-	stats.recordStat(node, wasError)
-
-	if wasError {
-		return err
+func errorMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		err := next(c)
+		if err != nil {
+			return error_templ.ExecuteTemplate(c.Response().Writer, "error", map[string]interface{}{
+				"Error": err.Error(),
+			})
+		}
+		return nil
 	}
-	return nil
-}
-
-var discprovUrls = []string{"http://node-0:26659", "http://node-1:26659", "http://node-2:26659", "http://node-3:26659", "http://node-4:26659", "http://node-5:26659", "http://node-6:26659"}
-
-func randomDiscprov() string {
-	randomIndex := rand.IntN(len(discprovUrls))
-	return discprovUrls[randomIndex]
 }
